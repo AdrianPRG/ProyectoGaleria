087e18ceeda7c85d7addcf606a5fd9ae0e78ef01
AdrianPRG <alopgal962@g.educaand.es>
2025-03-04 15:03:12 +0100

    inicial

---
+++ database/migrations/2025_02_27_132360_create_pieza_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pieza', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->enum('tipo', ['reloj', 'brujula','collar','pulsera','anillo','pendiente']);
+            $table->string('imagen');
+            $table->string('marca');
+            $table->string('modelo');
+            $table->integer('puntuacion');
+            $table->foreignId('categoria_id')->constrained('categoria');
+            $table->foreignId('fabricante_id')->constrained('fabricante');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pieza');
+    }
+};
