91cb3e33cb69c5d9b24829764f8760e14deb016e
AdrianPRG <alopgal962@g.educaand.es>
2025-03-09 16:23:36 +0100

    Mejor UI

--- resources/views/components/cabecera.blade.php
+++ resources/views/components/cabecera.blade.php
@@ -1,13 +1,13 @@
+<link rel="stylesheet" href="{{ asset('css/cabecera.css') }}">
 <header>
     <nav>
         <div class="logo">
-            <h1>Clickx</h1>
+            <h1><i>Clickx</i></h1>
         </div>
-        <ul class="nav-links">
+        <div class="nav-links">
             <li><a href="#inicio">Inicio</a></li>
-            <li><a href="#galeria">Galería</a></li>
-            <li><a href="#nosotros">Sobre Nosotros</a></li>
             <li><a href="#contacto">Contacto</a></li>
-        </ul>
+            <li><a href="#nosotros">Sobre Nosotros</a></li>
+        </div>
     </nav>
 </header>
--- resources/views/piezas.blade.php
+++ resources/views/piezas.blade.php
@@ -1,5 +1,6 @@
 <link href="{{ asset('css/piezas.css') }}" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+@include('components.cabecera')
 <div class="galeria">
     @foreach($piezas as $pieza)
         <div class="pieza">
